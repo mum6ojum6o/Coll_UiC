1f81f87cb7017d4d36464cc8c5cd1af3e51a4b92
mum6ojum6o <mundyarjan@gmail.com>
2017-02-21 15:38:54 -0600

    Without customer adapter for listview

--- Android/CS478_ProjectII/VideoPlaylist/app/src/main/java/com/cs478/arjan/createplaylist/VideoPlaylist.java
+++ Android/CS478_ProjectII/VideoPlaylist/app/src/main/java/com/cs478/arjan/createplaylist/VideoPlaylist.java
@@ -39,6 +39,7 @@ public class VideoPlaylist extends AppCompatActivity {
     int cbCheck;
     ListView lv;
     Songs[] songs;
+    ArrayList<String> songsII = new ArrayList<String>();
     CheckBoxAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,19 +48,42 @@ public class VideoPlaylist extends AppCompatActivity {
         myToolbar = (Toolbar) findViewById(R.id.activityToolbar);
         setSupportActionBar(myToolbar);
         lv = (ListView) findViewById(R.id.listview);
-        songs= new Songs[]{new Songs("Ecstacy Of Gold",R.drawable.ecstacy),new Songs("Dream On",R.drawable.aerosmith_dream),
+        songsII.add("Ecstacy Of Gold");
+        songsII.add("Dream On");
+        songsII.add("Fuel");
+        songsII.add("Stairway To Heaven");
+        songsII.add("High Hopes");
+        songsII.add("Don't Cry");
+        songsII.add("Soldier Of Fortune");
+        songsII.add("Hey You!");
+        songsII.add("Until it Sleeps!");
+       /* songs= new Songs[]{new Songs("Ecstacy Of Gold",R.drawable.ecstacy),new Songs("Dream On",R.drawable.aerosmith_dream),
                 new Songs("Fuel",R.drawable.reload),new Songs("Stairway To Heaven",R.drawable.sth_lz),
                 new Songs("Don't Cry",R.drawable.gnr_dc),new Songs("Soldier Of Fortune",R.drawable.dp_sof),
                 new Songs("High Hopes",R.drawable.pf_hy),new Songs("Hey You!",R.drawable.pf_hh_),
                 new Songs("Until it Sleeps!",R.drawable.load)};
-         adapter=new CheckBoxAdapter(this,songs);
-        lv.setAdapter(adapter);
-         Log.i("VideoPlaylist:", "In onCreate()");
+         adapter=new CheckBoxAdapter(this,songs); */
+        // lv.setAdapter(adapter);
+        lAdapter= new ArrayAdapter(this,R.layout.listview_layout,R.id.textView1,songsII);
+        lv.setAdapter(lAdapter);
+        Log.i("VideoPlaylist:", "In onCreate()");
+
+
     }
 //Displays the app bar menu
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.apptoobar,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+    public int checkboxCounter(){
+        int count=0;
+        CheckBox c;
+        for (int i=0;i<songsII.size();i++){
+            c=(CheckBox)lv.getChildAt(i).findViewById(R.id.checkbox);
+            if(c.isChecked())
+                count++;
+        }
+        return count;
     }
     //Actions to be taken on the basis of options selected in the ListView.
     public boolean onOptionsItemSelected(MenuItem item){
@@ -67,50 +91,54 @@ public class VideoPlaylist extends AppCompatActivity {
 
         switch(item.getItemId()){
             case R.id.action_favorite:
-                Log.i("VideoPlaylist:","In onOptionsItemsSelected;action_favorite:"+adapter.checkboxCount());
-                if (adapter.checkboxCount()==0)
+                //Log.i("VideoPlaylist:","In onOptionsItemsSelected;action_favorite:"+adapter.checkboxCount());
+                if (checkboxCounter()==0)
                     Toast.makeText(getApplicationContext(),"No Songs Selected",LENGTH_SHORT).show();
                 else{
-                ArrayList<String> ss=new ArrayList<String>();
+                    CheckBox c;
+                    ArrayList<String> ss=new ArrayList<String>();
+                /*ArrayList<String> ss=new ArrayList<String>();
                 for (int i=0;i<songs.length;i++){
                     CheckBox c = (CheckBox)lv.getChildAt(i).findViewById(R.id.checkbox);
                     if(c.isChecked()==true)
-                        ss.add(songs[i].getName());
-                }
+                        ss.add(songs[i].getName());*/
+                    for (int i=0;i<songsII.size();i++) {
+                        c = (CheckBox) lv.getChildAt(i).findViewById(R.id.checkbox);
+                        if(c.isChecked()==true)
+                            ss.add(songsII.get(i));
+                    }
+
                 Intent createPlaylist = new Intent(VideoPlaylist.this,Playlist.class);
                 createPlaylist.putExtra("selectedSongs",ss);
                     Log.i("VideoPlaylist:","Creating intent:");
                 startActivity(createPlaylist);
             }
                 return true;
-            case R.id.act_clear: for (int i=0;i<lv.getCount();i++){
+            case R.id.act_clear: for (int i=0;i<songsII.size();i++){
                 CheckBox c = (CheckBox)lv.getChildAt(i).findViewById(R.id.checkbox);
                 if (c.isChecked()){
                     c.setChecked(false);
-                    adapter.songsList[i].setChecked(false);
+
                 }
             }
                 return true;
-            case R.id.act_invert:for (int i=0;i<lv.getCount();i++) {
+            case R.id.act_invert:for (int i=0;i<songsII.size();i++) {
                 CheckBox c = (CheckBox) lv.getChildAt(i).findViewById(R.id.checkbox);
                 if (c.isChecked()) {
                     c.setChecked(false);
-                    adapter.songsList[i].setChecked(false);
-                    Log.i("Videoplaylist:","Checking checkboxes"+ adapter.songsList[i].getName());
-                } else {
+                 } else {
                     c.setChecked(true);
-                    adapter.songsList[i].setChecked(true);
                 }
             }
 
                 return true;
             case R.id.act_checkall:
 
-                for (int i=0;i<lv.getCount();i++){
+                for (int i=0;i<songsII.size();i++){
                     CheckBox c = (CheckBox)lv.getChildAt(i).findViewById(R.id.checkbox);
                     if(!c.isChecked()) {
                         c.setChecked(true);
-                        adapter.songsList[i].setChecked(true);
+                        //adapter.songsList[i].setChecked(true);
                     }
                 }
                 return true;
